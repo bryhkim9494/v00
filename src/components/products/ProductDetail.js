9a425b4f689d35a8e4d33e935af70f537a2ad7f0
bryhkim9494 <bryhkim9494@gmail.com>
2023-06-27 17:06:02 +0900

    리액트 컨테이너 컴포넌트

---
+++ src/components/products/ProductDetail.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const BlueDiv = styled.div`
+    background-color: blue
+`
+
+const ProductDetail = ({product}) => {
+
+    const {pno, pname, price} = product
+
+    
+    return ( 
+        <li>
+            <BlueDiv>PNO {pno}</BlueDiv>
+            <div>NAME {pname}</div>
+            <div>{price}</div>
+            <button>BUY</button>
+
+        </li>
+        
+     );
+}
+ 
+export default ProductDetail;
