273b267183f589f659d805bee015385d99ea14b9
bryhkim9494 <bryhkim9494@gmail.com>
2023-06-27 17:02:27 +0900

    리액트 시작

---
+++ src/components/HelloWorld2.js
@@ -0,0 +1,28 @@
+const HelloWorld2 = (props) => {
+    console.log(props)
+    return ( 
+        <>
+        <div>
+            Hello Wolrd2
+        </div>
+        <div>
+            Hello Wolrd2
+        </div>
+        </>
+     );
+}
+
+// const LoopEx = () => {
+
+//     const arr = [1,2,3,4,5,6]
+//     return ( 
+//         <ul>
+//             {arr.map(ele => <li>AAA</li>)}
+
+//         </ul>
+//      );
+// }
+ 
+// export default LoopEx;
+ 
+export default HelloWorld2;
